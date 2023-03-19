91d0cdfdf95e785c374eaa1212c0cf811bb93231
Adrian Prado <drixar@gmail.com>
2023-03-19 19:29:18 -0300

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <div>
+      <header className="App-header">
+        <a href='/'>e-commerce</a>
+      </header>
+      <main>
+        Lista de Productos
+      </main>
+    </div>
+  );
+}
+
+export default App;
