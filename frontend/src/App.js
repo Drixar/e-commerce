702eac0278d829890a25437d8b2a1571be87ae80
Adrian Prado <drixar@gmail.com>
2023-03-19 22:09:56 -0300

    lista de productos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,38 @@
-
+import data from './data';
 
 function App() {
   return (
     <div>
       <header className="App-header">
-        <a href='/'>e-commerce</a>
+        <a href="/">e-commerce</a>
       </header>
       <main>
-        Lista de Productos
+        <h1>Lista de Productos</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div
+              className="product"
+              key={product.slug}
+            >
+              <div class="img">
+                <a href={`/product/${product.slug}`}>
+                  <img
+                    src={product.imageb}
+                    alt={product.name}
+                  />
+                  {/* <img class="top" src= {product.imagef} alt={product.name}/> */}
+                </a>
+              </div>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p> {product.name} </p>
+                </a>
+                <p> {product.price} </p>
+                <button>Add to Cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
       </main>
     </div>
   );
---
+++ frontend/src/data.js
@@ -0,0 +1,58 @@
+const data = {
+    products: [
+        {
+            name: "Gopaldhara Darjeeling Romance",
+            slug: "Te-Gopaldhara-Darjeeling-Romance",
+            category: "Premium White Tea",
+            imageb: "/images/p1b.jpg",
+            imagef: "/images/p1f.jpg",
+            price: 435,
+            countInStock: 10,
+            brand: "Gopaldhara",
+            rating: 4.5,
+            numReviews: 1,
+            decription: "This is one of the refreshing and premium White Tea from Gopaldhara Tea Estate, cultivated in the picturesque slopes of the Darjeeling Hills at altitudes up to 7000 ft (2000 meter), which undergo gentle processing to produce this gourmet white tea."
+        },
+        {
+            name: "Darjeeling Green Tea Broken",
+            slug: "Te-Darjeeling-Green-Tea-Broken",
+            category: "Regular Series Tea",
+            imageb: "/images/p2b.jpg",
+            imagef: "/images/p2f.jpg",
+            price: 385,
+            countInStock: 10,
+            brand: "Darjeeling",
+            rating: 4.0,
+            numReviews: 1,
+            decription: "The Darjeeling green tea broken are made during the rolling and sorting of the tea. Since the tea has already been thoroughly steamed even though it is small in size it is still very pleasing to drink. "
+        },
+        {
+            name: "Rohini Autumn Moonlight White Oolong",
+            slug: "Te-Rohini-Autumn-Moonlight-White-Oolong",
+            category: "Classic Series Tea",
+            imageb: "/images/p3b.jpg",
+            imagef: "/images/p3f.jpg",
+            price: 450,
+            countInStock: 10,
+            brand: "Rohini",
+            rating: 4.2,
+            numReviews: 1,
+            decription: "This is a delicate & refreshing white oolong tea from Rohini Tea Estate, we have named this tea Moonlight White Oolong because it is mostly processed like any other white tea except that it is fixed & gently rolled at the end. "
+        },
+        {
+            name: "Darjeeling Earl Grey Black",
+            slug: "Te-Darjeeling-Earl-Grey-Black",
+            category: "Regular Series Tea",
+            imageb: "/images/p4b.jpg",
+            imagef: "/images/p4f.jpg",
+            price: 575,
+            countInStock: 10,
+            brand: "Darjeeling",
+            rating: 4.5,
+            numReviews: 1,
+            decription: "The Earl Grey blend is named after the 2nd Earl Grey, the British Prime Minister in 1830. This Darjeeling Earl Grey Black Broken tea is a tea blend with a distinctive flavor and aroma derived from the addition of oil extracted from the rind of the bergamot orange"
+        }
+    ]
+} 
+
+export default data;
